de2c8df12930356a2821299c2bb6e6c81ab349e9
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2022-08-24 16:29:50 +0530

    Update chronicle_engine.py

--- chronicle_engine.py
+++ chronicle_engine.py
@@ -1,19 +1,25 @@
 import datetime
 import os
 import sys
+import time
+import sounddevice as sd
+from scipy.io.wavfile import write
 
 date = datetime.datetime.now().strftime("%h:%H:%M:%S")
 act_log = str(date).replace(":", "-") + "-Log.txt"
 audio_log = str(date).replace(":", "-") + "-Audio.wav"
 
-folder = "Casper Log"
+#first of all create a folder named Activity Log
+folder = "Activity Log"
 save_path = os.path.join(folder, act_log)
-folderB = "Casper Log"
+
+folderB = "Activity Log"
 save_pathB = os.path.join(folder, audio_log)
 
 
-def casper_log(write, var):
-    if var == 0:
+def chronicle_log(write, stealth):
+    #enabled activity logging
+    if stealth == 0:
         print_save = sys.stdout
         f = open(save_path, 'a', encoding="utf-8")
         sys.stdout = f
@@ -22,11 +28,42 @@ def casper_log(write, var):
         sys.stdout = print_save
         f.close()
 
-    elif var == 1:
+    #disabled activity logging
+    elif stealth == 1:
         print_save = sys.stdout
         f = open(save_path, 'a', encoding="utf-8")
         sys.stdout = f
         y = write
         pass
+        #activity logging diabled
         sys.stdout = print_save
         f.close()
+
+
+def chronicle_audio(dur):
+    print("<<< INITIATED CHRONICLE AUDIO MODE >>>")
+    print("(●) Recording...")
+
+    frequency = 44100
+    duration = dur #records for given amount of mins/secs
+    record_audio = sd.rec(int(duration * frequency), samplerate=frequency, channels=2)
+    sd.wait()
+    write(save_pathB, frequency, record_audio)
+    print("Recoding audio completed")
+
+
+def clean_slate():
+    print("<<< INITIATED CLEAN SLATE PROTOCOL >>>")
+
+    mydir = "Activity Log"
+    txtfiles = [f for f in os.listdir(mydir) if f.endswith(".txt")]
+    wavfiles = [f for f in os.listdir(mydir) if f.endswith(".wav")]
+    for f in txtfiles:
+        os.remove(os.path.join(mydir, f))
+
+    for f in wavfiles:
+        os.remove(os.path.join(mydir, f))
+
+    time.sleep(1)
+    print("All activity log files has been deleted")
+    #deletes all activity log files in the folder
